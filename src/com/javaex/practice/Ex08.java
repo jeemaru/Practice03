cbd171aed8bc9baa4c5ad186b88adeea2e5a9563
dkdzm <dkdzm@1.230.218.24>
2022-04-19 02:18:47 +0900

    practice03_ hw18

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -5,15 +5,15 @@ public class Ex08 {
 	public static void main(String[] args) {
 		
 		
-		for(int i=2; i<=9; i++) {
-			for(int x=1; x<=9; x++) {
+		for(int x=1; x<=9; x++) {
+			for(int i=2; i<=9; i++) {
 				System.out.print(i+"*"+x+"="+(i*x)+"  ");
 				
 			}System.out.println("");
 		}
 		
 		
-
+		
 	}
 
 }
